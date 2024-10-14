1d13df8b25a3f7c66525524c0b464ae406ea33a2
Qfaizan <mohamedfaizan94@gmail.com>
2024-10-14 19:13:03 +0530

    adds

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,195 +1,233 @@
-import { Box, Divider, Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Divider,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import React from "react";
-import vmap from '../coupleimg.png';
-import map from '../thanks.jpg';
+import coupleImage from '../coupleimg.png'; // Couple Image
+import qrCodeImage from '../qrcode.png';   // QR Code Image
 
-const MyW = () => {
+const WeddingInvitation = () => {
   return (
     <>
-      {/* Invitation Header */}
+      {/* Main Heading */}
+      <Box
+        sx={{
+          textAlign: "center",
+          background: "linear-gradient(to right, #FFD5E5, #FFF0F5)",
+          padding: "20px",
+        }}
+      >
+          {/* Islamic Greeting & Verse */}
+          <Typography
+          sx={{
+            fontSize: { xs: "16px", md: "20px" },
+            color: "#D47474",
+            fontStyle: "italic",
+            marginBottom: 2,
+          }}
+        >
+          "And among His signs is that He created for you mates from among yourselves, 
+          <br />
+          that you may dwell in tranquility with them..."
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: { xs: "28px", md: "36px" },
+            fontWeight: "bold",
+            color: "#8B0000",
+            fontFamily: "'Dancing Script', cursive",
+          }}
+        >
+           
+        </Typography>
+       
+      </Box>
+
+      {/* Couple Image and Greeting */}
       <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
         flexDirection="column"
-        sx={{
-          width: "100%",
-          background: "linear-gradient(to bottom right, #ffcccb, #fff0f5)",  // Soft gradient for background
-          textAlign: "center",
-          padding: 2,
-          fontFamily: "'Dancing Script'", // A script font for elegance
-        }}
+        sx={{ width: "100%", padding: "20px", textAlign: "center" }}
       >
-        <Typography
-          sx={{
-            fontSize: { xs: "10px", md: "12px" },
-            color: "#d47474",
+        {/* Reduced Couple Image */}
+        <img
+          src={coupleImage}
+          alt="Couple"
+          style={{
+            width: "100%",
+            maxWidth: "250px", // Smaller for mobile
+            borderRadius: "10px",
           }}
-        >
-          In the name of Allah, the Most Gracious, the Most Merciful
-        </Typography>
-        
-        <Typography
-          sx={{
-            fontSize: { xs: "20px", md: "28px" },
-            marginTop: 1,
-            color: "grey",
-            fontFamily: "'Open Sans', sans-serif",
-          }}
-        >
-          We are glad to invite you for the marriage of
-        </Typography>
+        />
+
+        {/* Greeting Quote */}
+        <Box sx={{ textAlign: "center", marginTop: "15px" }}>
+          <Typography
+            sx={{
+              fontSize: { xs: "20px", md: "24px" },
+              fontWeight: "bold",
+              color: "#8B0000",
+            }}
+          >
+            "Two Souls, One Heart"
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: { xs: "16px", md: "18px" },
+              color: "#696969",
+              marginTop: 1,
+            }}
+          >
+            We invite you to join us as we embark on this journey of love and togetherness (Nikah).
+          </Typography>
+        </Box>
       </Box>
 
-      {/* Couple Names */}
+      {/* Faizan & Arshiya Names in Box */}
       <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection={{ xs: "column", md: "row" }}
         sx={{
-          width: "100%",
-          height: "70vh",
+          width: "80%",
+          margin: "auto",
+          padding: "15px",
+          backgroundColor: "#FFF5F7",
+          borderRadius: "10px",
+          boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
           textAlign: "center",
-          padding: 1.8,
         }}
       >
-        {/* Name Faizan */}
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
+        <Typography
           sx={{
-            marginBottom: { xs: 2, md: 0 },
-            fontFamily: "'Great Vibes', cursive", // Elegant font for names
-            fontSize: { xs: "60px", md: "100px" },
-            color: "#d47474",
+            fontSize: { xs: "20px", md: "24px" },
+            color: "#8B0000",
+            fontWeight: "bold",
           }}
         >
           Faizan
-        </Box>
-
-        {/* Divider/Decoration between Names */}
-        <Divider
+        </Typography>
+        <Typography
           sx={{
-            width: "70%",
-            maxWidth: { xs: "200px", md: "400px" },
-            borderBottomWidth: 2,
-            borderColor: "#d47474",
-            marginY: 3,
+            fontSize: { xs: "16px", md: "20px" },
+            color: "#696969",
+            marginTop: "10px",
           }}
         >
-          <Typography sx={{ fontSize: { xs: "24px", md: "40px" }, color: "#d47474", fontFamily: "'Great Vibes', cursive" }}>
-            &amp;
-          </Typography>
-        </Divider>
-
-        {/* Name Arshiya */}
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
+          Son of Mrs.K B Nazeer Ahmed
+        </Typography>
+        <Divider sx={{ marginY: 2 }} />
+        <Typography
           sx={{
-            marginTop: { xs: 2, md: 0 },
-            fontFamily: "'Great Vibes', cursive",
-            fontSize: { xs: "58px", md: "100px" },
-            color: "#d47474",
+            fontSize: { xs: "20px", md: "24px" },
+            color: "#8B0000",
+            fontWeight: "bold",
           }}
         >
           Arshiya
-        </Box>
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: { xs: "16px", md: "20px" },
+            color: "#696969",
+            marginTop: "10px",
+          }}
+        >
+          Daughter of Mr. & Mrs. Alahuddin
+        </Typography>
       </Box>
 
-      {/* Image Section with Text */}
-      <Box sx={{ textAlign: "center", marginTop: { xs: 3, md: 6 } }}>
+      {/* Accordion Section */}
+      <Box sx={{ width: "100%", marginTop: 3, paddingX: { xs: 2, md: 5 } }}>
+        {/* Accordion: When & Where */}
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography
+              sx={{ fontWeight: "bold", fontSize: { xs: "18px", md: "22px" } }}
+            >
+              When & Where?
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box sx={{ textAlign: "center" }}>
+              {/* Nikah Ceremony */}
+              <Typography
+                sx={{
+                  fontSize: { xs: "20px", md: "24px" },
+                  color: "#8B0000",
+                  fontWeight: "bold",
+                  marginBottom: 2,
+                }}
+              >
+                Nikah Ceremony
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: { xs: "16px", md: "18px" },
+                  color: "#696969",
+                  marginBottom: 1,
+                }}
+              >
+                Sunday, November 17, 2024 @ 11:55 AM
+                <br />
+                Neelifield Masjid, Ambur
+              </Typography>
+
+              <Divider sx={{ marginY: 2 }} />
+
+              {/* Valima Reception */}
+              <Typography
+                sx={{
+                  fontSize: { xs: "20px", md: "24px" },
+                  color: "#8B0000",
+                  fontWeight: "bold",
+                  marginBottom: 2,
+                }}
+              >
+                Valima Reception
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: { xs: "16px", md: "18px" },
+                  color: "#696969",
+                }}
+              >
+                Onsame day After Zohar Namaz
+                <br />
+                Jalalia Function Hall, Ambur
+              </Typography>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+
+      {/* QR Code Section */}
+      <Box sx={{ textAlign: "center", marginTop: 3 }}>
+        <Typography
+          sx={{
+            fontSize: { xs: "16px", md: "18px" },
+            color: "#8B0000",
+            fontWeight: "bold",
+          }}
+        >
+          Scan to View Location
+        </Typography>
         <img
-          src={vmap}
-          alt="Couple Imae"
+          src={qrCodeImage}
+          alt="QR Code"
           style={{
-            width: "110%",
-            maxWidth: "320px",
-            height: "auto",
-            borderRadius: "10px",
-            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+            width: "100px",
+            height: "100px",
+            marginTop: "10px",
           }}
         />
- <Box  sx={{ border:'2px solid grey',borderRadius:'25px'}}>
-        {/* Invitation Text */}
-        <Typography
-          sx={{
-            color: "black",
-            fontSize: { xs: "16px", md: "22px" },
-            marginTop: 3,
-            fontFamily: "'Dancing Script', cursive", // A celebratory script font
-          }}
-        >
-          Solicits your gracious presence on the occasion 
-        </Typography>
-
-        {/* Date & Venue */}
-        <Typography
-          sx={{
-            color: "#d47474",
-            fontSize: { xs: "18px", md: "24px" },
-            marginTop: 2,
-            fontFamily: "'Open Sans', sans-serif",
-          }}
-        >
-          Nikah 
-        </Typography>
-        
-        {/* Map Image */}
-       
-        <Typography
-          sx={{
-            color: "black",
-            fontSize: { xs: "16px", md: "22px" },
-            marginTop: 3,
-            fontFamily: "'Dancing Script', cursive", // A celebratory script font
-          }}
-        >
-          on sunday November 17,2024 @ 11.55 am sharp at Neelifield Masjid,Ambur
-        </Typography>
-
-        <Typography
-          sx={{
-            color: "#d47474",
-            fontSize: { xs: "18px", md: "24px" },
-            marginTop: 2,
-            fontFamily: "'Open Sans', sans-serif",
-          }}
-        >
-          Valima 
-        </Typography>
-        <Typography
-          sx={{
-            color: "black",
-            fontSize: { xs: "16px", md: "22px" },
-            marginTop: 3,
-            fontFamily: "'Dancing Script', cursive", // A celebratory script font
-          }}
-        >
-          on same day after zohar namaz at jalalia function hall,Near Mazhar Uloom High School,Ambur
-        </Typography>
-        </Box>
-          <img
-            src={map}
-            alt="Map"
-            style={{
-              width: "110%",
-              // maxWidth: "300px",
-              height: "auto",
-              marginTop: 20,
-              borderRadius: "10px",
-              boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
-            }}
-          />
-        
       </Box>
     </>
   );
 };
 
-export default MyW;
+export default WeddingInvitation;
